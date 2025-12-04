48d8e71ef094b8533bffb9189eb207e38f20c115
Ángel Gandarias <160829407+angelgandarias@users.noreply.github.com>
2025-12-04 11:52:52 +0100

    Traceback

--- humeadPrueba.py
+++ humeadPrueba.py
@@ -11,3 +11,18 @@ while True:
     humedad = bme.read_humidity()   # %
     print(f"Humedad: {humedad:.2f}%")
     time.sleep(1)
+
+
+
+'''Traceback (most recent call last):
+  File "/home/pi/Desktop/ClimaStation/Pruebas/pruebaHumedd.py", line 8, in <module>
+    bme = Adafruit_BME280.BME280(address=0x76)
+  File "/home/pi/Desktop/ClimaStation/venv/lib/python3.13/site-packages/Adafruit_BME280/BME280.py", line 131, in __init__
+    self._device = i2c.get_i2c_device(address, **kwargs)
+                   ~~~~~~~~~~~~~~~~~~^^^^^^^^^^^^^^^^^^^
+  File "/home/pi/Desktop/ClimaStation/venv/lib/python3.13/site-packages/Adafruit_GPIO/I2C.py", line 63, in get_i2c_device
+    busnum = get_default_bus()
+  File "/home/pi/Desktop/ClimaStation/venv/lib/python3.13/site-packages/Adafruit_GPIO/I2C.py", line 55, in get_default_bus
+    raise RuntimeError('Could not determine default I2C bus for platform.')
+RuntimeError: Could not determine default I2C bus for platform.
+'''
